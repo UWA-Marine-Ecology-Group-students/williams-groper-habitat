225a471f8576546fc5619d7e48f18d8a43dbf053
Hannah Williams <hannah.williams63@live.co.uk>
2025-08-01 09:19:18 +0800

    length extractions

--- r/bait_comp_analysis/1.5 length data extraction.R
+++ r/bait_comp_analysis/1.5 length data extraction.R
@@ -21,12 +21,45 @@ name <- "2024_Wudjari_bait_comp" #set study name
 metadata <- readRDS("./data/tidy/2024_Wudjari_bait_comp_Metadata.rds") %>%
   glimpse()
 
-
-
-
-em_length3dpoints <- read_em_length(here::here("r-workflows/data/raw/")) %>%                   
-  dplyr::select(-c(comment))%>% # there is a comment column in metadata, so you will need to remove this column from EM data
-  dplyr::inner_join(metadata, by = join_by(sample, campaignid)) %>%
+length <- read_em_length(here::here("./data/raw/bait_comp/em export/")) %>%  
+  dplyr::inner_join(metadata, by = join_by(opcode)) %>%
   dplyr::filter(successful_length %in% "Yes") %>%
-  # dplyr::rename(length_mm = length) %>%
   glimpse() 
+
+#remove sync points
+unique(length$comment) # 'sync', 'SYNC'
+test <- length %>%
+  dplyr::filter(!comment %in% c("sync", "SYNC"))
+
+unique(test$comment)
+
+
+
+
+######## TODO: Think about maxn.all data frame & maxn.stage (all)
+
+#format & add zeros
+tidy_lengths <- length %>%
+  dplyr::mutate(family = ifelse(is.na(family), 'Labridae', family))%>%
+  dplyr::mutate(genus = ifelse(is.na(genus), 'Achoerodus', genus))%>%
+  dplyr::mutate(species = ifelse(is.na(species), 'gouldii', species))%>%
+  dplyr::mutate(number = as.numeric(number)) %>%
+  tidyr::uncount(number) %>%
+  dplyr::mutate(number = 1) %>% 
+  # Add in missing samples
+  dplyr::right_join(metadata) %>%
+  dplyr::filter(successful_length %in% "Yes") %>%
+  # Complete the data (add in zeros for every species)
+  dplyr::select(campaignid, sample, family, genus, species, length_mm, number, any_of(c("range", "rms", "precision"))) %>% # this will keep EM only columns
+  tidyr::complete(nesting(campaignid, sample), nesting(family, genus, species)) %>%
+  tidyr::replace_na(list(number = 0)) %>%
+  ungroup() %>%
+  dplyr::filter(!is.na(number)) %>%
+  dplyr::mutate(length_mm = as.numeric(length_mm)) %>%
+  left_join(., metadata) %>%
+  glimpse()
+
+
+
+
+#maxn.stage_lengths
